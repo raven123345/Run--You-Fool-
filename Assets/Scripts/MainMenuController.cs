2ae9c7efce363d00fb08686073c64ffb108df803
Radoslav Banev <banev.radoslav@gmail.com>
2021-03-01 22:26:49 +0200

    Работа по подменютата_01
    Добавяне на Help панел; Options без опции.

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -2,16 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
+    [SerializeField]
+    GameObject helpPanel;
+    [SerializeField]
+    GameObject optionsPanel;
     private void Start()
     {
-
+        //CloseHelp();
     }
     public void LoadGameScene()
     {
         SceneManager.LoadScene("Level", LoadSceneMode.Single);
+    }
+
+    public void LoadPanel(Button button)
+    {
+        button.gameObject.transform.parent.gameObject.SetActive(true);
+    }
+
+    public void ClosePanel(Button button)
+    {
+        button.gameObject.transform.parent.gameObject.SetActive(false);
     }
 
     public void LoadMainMenu()
